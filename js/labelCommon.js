a756774cdade000a6b1f01c2eb4d8d91f5deaf6c
duanhongwei929 <1250101915@qq.com>
2020-06-30 11:36:14 +0800

    a

--- js/labelCommon.js
+++ js/labelCommon.js
@@ -7,7 +7,7 @@ document.write('' +
     '<link rel="stylesheet" type="text/css" href="css/swiper.min.css"/>\n' +
 
     /* js */
-    '<script src="js/phone.js"></script>\n' +
+    '<script src="js/phone.js"></script>\n' + /* rem */
     '<script src="js/swiper.min.js"></script>\n' +
     '<script src="js/vue.min.js"></script>\n' +
     '<script src="js/axios.min.js"></script>\n' +
