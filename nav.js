2315786bae7febd109c1cd22c0d95e2042b4d75a
duanhongwei929 <1250101915@qq.com>
2020-06-30 11:53:34 +0800

    a

---
+++ nav.js
@@ -0,0 +1,15 @@
+document.writeln("<div class=\'nav\'>");
+document.writeln("    <div class=\'content nav-box\'>");
+document.writeln("        <div class=\'logo\'>");
+document.writeln("            <img src=\'images/logo.png\' alt=\'\'>");
+document.writeln("        </div>");
+document.writeln("        <ul>");
+document.writeln("            <li class=\'choose\'>首页</li>");
+document.writeln("            <li>高端定制</li>");
+document.writeln("            <li>移动应用</li>");
+document.writeln("            <li>行业方案</li>");
+document.writeln("            <li>关于我们</li>");
+document.writeln("        </ul>");
+document.writeln("        <div class=\'phone\'>123456789</div>");
+document.writeln("    </div>");
+document.writeln("</div>");
