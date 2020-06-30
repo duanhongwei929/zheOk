2f9de07ce9e2298b5a2dc03d09a1ee69e2e3d309
qys <949817375@qq.com>
2020-06-30 12:08:18 +0800

    qu

--- nav.js
+++ nav.js
@@ -4,16 +4,23 @@ document.writeln("        <div class=\'logo\'>");
 document.writeln("            <img src=\'images/logo.png\' alt=\'\'>");
 document.writeln("        </div>");
 document.writeln("        <ul>");
-document.writeln("            <li class=\'choose\'>首页</li>");
-document.writeln("            <li>高端定制</li>");
-document.writeln("            <li>移动应用</li>");
-document.writeln("            <li>行业方案</li>");
-document.writeln("            <li>关于我们</li>");
+document.writeln("            <li class=\'choose\'><a href='index.html'>首页</a></li>");
+document.writeln("            <li><a href='about.html'>高端定制</a></li>");
+document.writeln("            <li><a href='about.html'>移动应用</a></li>");
+document.writeln("            <li><a href='about.html'>行业方案</a></li>");
+document.writeln("            <li><a href='about.html'>关于我们</a></li>");
 document.writeln("        </ul>");
 document.writeln("        <div class=\'phone\'>123456789</div>");
 document.writeln("    </div>");
 document.writeln("</div>");
-
+$(document).ready(function() {
+	$(".nav-box li a").each(function() {
+		if ($(this)[0].href == String(window.location)) {
+			$(".nav-box .choose").removeClass('choose')
+			$(this).parent().addClass("choose");
+		}
+	});
+});
 
 /*---- 备份 -----*/
 /*
