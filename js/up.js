b1c4d5644779fab142b3f137b745d831b9a41828
duanhongwei <1250101915@qq.com>
2020-07-05 22:10:25 +0800

    a

---
+++ js/up.js
@@ -0,0 +1,53 @@
+
+!function (){
+    // 添加 offset 类
+    var specialTags = document.querySelectorAll('[data-x]')
+
+    for(let i =0;i<specialTags.length; i++){
+        specialTags[i].classList.add('offset')
+    }
+    console.log('ccc')
+    findClosestAndRemoveOffset()
+    console.log('bbb')
+    window.addEventListener('scroll', function(x){
+        findClosestAndRemoveOffset()
+    })
+
+
+    /* helper */
+    function findClosestAndRemoveOffset(){
+        let specialTags = document.querySelectorAll('[data-x]')
+
+        let minIndex = 0
+        for(let i =1;i<specialTags.length; i++){
+            if(Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)){
+                minIndex = i
+            }
+        }
+        // minIndex 就是里窗口顶部最近的元素
+        specialTags[minIndex].classList.remove('offset')
+        let id = specialTags[minIndex].id
+        /*console.log(id)
+        console.log(1)
+        let a = document.querySelector('a[href="#'+ id + '"]')
+        console.log(a)
+        console.log(2)
+        let li = a.parentNode
+        console.log(3)
+        let brothersAndMe = li.parentNode.children
+        console.log(4)
+        for(let i=0; i<brothersAndMe.length; i++){
+            brothersAndMe[i].classList.remove('highlight')
+        }
+        li.classList.add('highlight')*/
+    }
+    let liTags = document.querySelectorAll('nav.menu > ul > li')
+    for(let i=0; i<liTags.length; i++){
+        liTags[i].onmouseenter = function(x){
+            x.currentTarget.classList.add('active')
+        }
+        liTags[i].onmouseleave = function(x){
+            x.currentTarget.classList.remove('active')
+        }
+    }
+}.call()
